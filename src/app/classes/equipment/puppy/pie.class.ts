75bd5a4ec1fc11908658d003d0289b1b3585a200
APorter39 <aporter39@gatech.edu>
2024-09-01 20:22:47 -0500

    Auto-generated commit [ci skip]

--- src/app/classes/equipment/puppy/pie.class.ts
+++
@@ -1,8 +0,0 @@
-import { AbilityService } from "../../../services/ability.service";
-import { LogService } from "../../../services/log.servicee";
-import { Equipment, EquipmentClass } from "../../equipment.class";
-
-export class Pie extends Equipment {
-    name = 'Pie';
-    equipmentClass: EquipmentClass = 'beforeStartOfBattle';
-}
